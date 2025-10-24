d0f0402b1893b8cbbd18a678ed33daa2c387cb22
Faris Hassan <nhfarishassan2005@gmail.com>
2025-10-23 22:33:14 -0400

    added unit tests for constructors and methods for card class

--- uno project/tests/CardTest.java
+++ uno project/tests/CardTest.java
@@ -1,2 +1,38 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * JUnit tests for the Card class
+ *
+ * @author Faris Hassan 101300683
+ * @version 1.0
+ */
+
 public class CardTest {
+
+    @Test
+    public void testCardConstructorAndGettersColored() {
+        Card card = new Card(Card.Color.RED, Card.Value.FIVE);
+        assertEquals(Card.Color.RED, card.getColor());
+        assertEquals(Card.Value.FIVE, card.getValue());
+    }
+
+    @Test
+    public void testCardConstructorAndGettersWild() {
+        Card card = new Card(Card.Color.WILD, Card.Value.WILD_DRAW_TWO);
+        assertEquals(Card.Color.WILD, card.getColor());
+        assertEquals(Card.Value.WILD_DRAW_TWO, card.getValue());
+    }
+
+    @Test
+    public void testToStringColored() {
+        Card card = new Card(Card.Color.BLUE, Card.Value.SKIP);
+        assertEquals("BLUE SKIP", card.toString());
+    }
+
+    @Test
+    public void testToStringWild() {
+        Card card = new Card(Card.Color.WILD, Card.Value.WILD);
+        assertEquals("WILD", card.toString());
+    }
 }
