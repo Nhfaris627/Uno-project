801bae852e644097a7895e4fb7fb7f550bbba3fa
Nicky Fang <nfang3885@gmail.com>
2025-11-08 20:35:21 -0500

    Added GameModel class and GameModelListener interface. Began work on GameController

--- uno project/src/GameController.java
+++ uno project/src/GameController.java
@@ -1,7 +1,35 @@
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+/**
+ * COntroller for uno game implementing MVC pattern
+ * Mediates between GameModel and GameView
+ * Handles user input via ActionListener and updates the view via GameModelListener
+ *
+ * @author Nicky Fang 101304731
+ *
+ */
 public class GameController implements ActionListener, GameModelListener {
+
+    //
+    private GameModel model;
+    private GameView view;
+
+    /**
+     *
+     * @param model The game model
+     * @param view The game view
+     */
+    public GameController(GameModel model, GameView view) {
+        this.model = model;
+        this.view = view;
+
+        //add this controller to the model
+        //add this controller to the view
+        model.addListener(this);
+        view.bindController(this);
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
 
--- uno project/src/GameModelListener.java
+++ uno project/src/GameModelListener.java
@@ -1,2 +1,22 @@
+/**
+ *
+ * Listener interface for GameModel events
+ * Implements the observer pattern to notify views and controllers of game state changes
+ * @author Nicky Fang 101304731
+ *
+ */
+
 public interface GameModelListener {
+    public void onGameInitialized(GameState state);
+
+    public void onStateUpdated(GameState state);
+
+    public void onTurnAdvanced(GameState state);
+
+    public void onRoundWon(String winnerName, int pointsAwarded, GameState state);
+
+    public void onGameWon(String winnerName, int finalScore, GameState state);
+
+    public void onError(String errorMessage);
 }
+
