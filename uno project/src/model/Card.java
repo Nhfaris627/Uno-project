1883c9b8667684fedecdc1bc2bf2302527724aa4
Faris Hassan <nhfarishassan2005@gmail.com>
2025-11-23 14:20:38 -0500

    added dark side cards to deck and added flip functionality for light and dark side cards

--- uno project/src/model/Card.java
+++ uno project/src/model/Card.java
@@ -14,23 +14,78 @@ package model;
 public class Card {
 
     public enum Color{
-        RED, BLUE, GREEN, YELLOW, WILD
+        RED, BLUE, GREEN, YELLOW, WILD,
+        TEAL, PURPLE, PINK, ORANGE // flip side colors
     }
 
     public enum Value{
         ZERO, ONE, TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, SKIP, REVERSE,
-        DRAW_ONE, WILD, WILD_DRAW_TWO
+        DRAW_ONE, WILD, WILD_DRAW_TWO,
+        FLIP, DRAW_FIVE, SKIP_EVERYONE, WILD_DRAW_COLOR
+    }
+
+    public enum Side {
+        LIGHT, DARK
     }
 
     private Color color;
     private final Value value;
+    private Side currentSide;
+
+    // Store both sides of the card for flipping
+    private final Color lightColor;
+    private final Value lightValue;
+    private final Color darkColor;
+    private final Value darkValue;
 
     /**
      * Creates a card with given color and value
      */
-    public Card(Color color, Value value){
-        this.color = color;
-        this.value = value;
+    public Card(Color color, Value value) {
+        this(color, value, color, value, Side.LIGHT);
+    }
+
+    /**
+     * Creates a card with both light and dark sides
+     * @param lightColor Color on light side
+     * @param lightValue Value on light side
+     * @param darkColor Color on dark side
+     * @param darkValue Value on dark side
+     * @param initialSide Which side is initially facing up
+     */
+    public Card(Color lightColor, Value lightValue, Color darkColor, Value darkValue, Side initialSide) {
+        this.lightColor = lightColor;
+        this.lightValue = lightValue;
+        this.darkColor = darkColor;
+        this.darkValue = darkValue;
+        this.currentSide = initialSide;
+
+        // Set current color and value based on initial side
+        if (initialSide == Side.LIGHT) {
+            this.color = lightColor;
+            this.value = lightValue;
+        } else {
+            this.color = darkColor;
+            this.value = darkValue;
+        }
+    }
+
+    public void flip() {
+        if (currentSide == Side.LIGHT) {
+            currentSide = Side.DARK;
+            this.color = darkColor;
+        } else {
+            currentSide = Side.LIGHT;
+            this.color = lightColor;
+        }
+    }
+
+    /**
+     * Get the current side of the card
+     * @return the current side (LIGHT or DARK)
+     */
+    public Side getCurrentSide() {
+        return currentSide;
     }
 
     /**
@@ -78,17 +133,21 @@ public class Card {
             case DRAW_ONE: return 10;
             case WILD: return 40;
             case WILD_DRAW_TWO: return 50;
+            // Uno flip card values
+            case FLIP: return 20;
+            case DRAW_FIVE: return 20;
+            case SKIP_EVERYONE: return 30;
+            case WILD_DRAW_COLOR: return 60;
             default: return 0;
         }
     }
 
     @Override 
     public String toString(){
-        if (color == Color.WILD){
+        if (color == Color.WILD) {
             return value.toString();
         }
-
-        return color + " " + value;
+        return color + " " + value + " [" + currentSide + " ]";
     }
 }
 
--- uno project/src/model/Deck.java
+++ uno project/src/model/Deck.java
@@ -28,35 +28,82 @@ public class Deck {
     }
 
     /**
-     * Initializes a deck with 108 UNO cards
+     * Initializes a deck with UNO flip cards light and dark sides
      */
     private void initializeDeck() {
-        // Add number cards for each color
+        // Light side cards
         for (Card.Color color : new Card.Color[]{Card.Color.RED, Card.Color.BLUE, 
                                                    Card.Color.GREEN, Card.Color.YELLOW}) {
+
+            // Map light colors to dark colors
+            Card.Color darkColor = mapLightToDarkColor(color);
+
             // One zero per color
-            cards.add(new Card(color, Card.Value.ZERO));
-            
+            cards.add(new Card(color, Card.Value.ZERO, darkColor, Card.Value.FIVE, Card.Side.LIGHT));
+
             // Two of each 1-9 per color
-            for (Card.Value value : new Card.Value[]{Card.Value.ONE, Card.Value.TWO, 
-                    Card.Value.THREE, Card.Value.FOUR, Card.Value.FIVE, Card.Value.SIX,
-                    Card.Value.SEVEN, Card.Value.EIGHT, Card.Value.NINE}) {
-                cards.add(new Card(color, value));
-                cards.add(new Card(color, value));
+            Card.Value[] numbers = {Card.Value.ONE, Card.Value.TWO, Card.Value.THREE,
+                    Card.Value.FOUR, Card.Value.FIVE, Card.Value.SIX,
+                    Card.Value.SEVEN, Card.Value.EIGHT, Card.Value.NINE};
+
+            for (Card.Value value : numbers) {
+                // Each number maps to a dark side number
+                Card.Value darkValue = mapLightToDarkValue(value);
+                cards.add(new Card(color, value, darkColor, darkValue, Card.Side.LIGHT));
+                cards.add(new Card(color, value, darkColor, darkValue, Card.Side.LIGHT));
             }
-            
-            // Two of each action card per color
-            cards.add(new Card(color, Card.Value.SKIP));
-            cards.add(new Card(color, Card.Value.SKIP));
-            cards.add(new Card(color, Card.Value.REVERSE));
-            cards.add(new Card(color, Card.Value.REVERSE));
+
+            // Two skip cards per color and per side, light and dark
+            cards.add(new Card(color, Card.Value.SKIP, darkColor, Card.Value.SKIP, Card.Side.LIGHT));
+            cards.add(new Card(color, Card.Value.SKIP, darkColor, Card.Value.SKIP, Card.Side.LIGHT));
+
+            // Two reverse cards per color and side
+            cards.add(new Card(color, Card.Value.REVERSE, darkColor, Card.Value.REVERSE, Card.Side.LIGHT));
+            cards.add(new Card(color, Card.Value.REVERSE, darkColor, Card.Value.REVERSE, Card.Side.LIGHT));
+
+            // Two draw one cards per color (maps to DRAW_FIVE on dark side)
+            cards.add(new Card(color, Card.Value.DRAW_ONE, darkColor, Card.Value.DRAW_FIVE, Card.Side.LIGHT));
+            cards.add(new Card(color, Card.Value.DRAW_ONE, darkColor, Card.Value.DRAW_FIVE, Card.Side.LIGHT));
+
+            // Two flip cards per color (maps to FLIP on dark side)
+            cards.add(new Card(color, Card.Value.FLIP, darkColor, Card.Value.FLIP, Card.Side.LIGHT));
+            cards.add(new Card(color, Card.Value.FLIP, darkColor, Card.Value.FLIP, Card.Side.LIGHT));
         }
         
-        // Add 4 Wild cards and 4 Wild Draw Two cards
+        // Add 4 Wild cards (map to WILD on dark side)
         for (int i = 0; i < 4; i++) {
-            cards.add(new Card(Card.Color.WILD, Card.Value.WILD));
-            cards.add(new Card(Card.Color.WILD, Card.Value.WILD_DRAW_TWO));
+            cards.add(new Card(Card.Color.WILD, Card.Value.WILD,
+                        Card.Color.WILD, Card.Value.WILD, Card.Side.LIGHT));
+
         }
+
+        // Add 4 Wild Draw Two cards (map to WILD_DRAW_COLOR on dark side)
+        for (int i = 0; i < 4; i++) {
+            cards.add(new Card(Card.Color.WILD, Card.Value.WILD_DRAW_TWO,
+                        Card.Color.WILD, Card.Value.WILD_DRAW_COLOR, Card.Side.LIGHT));
+        }
+    }
+
+
+    /**
+     * Maps a light side color to its corresponding dark side color
+     */
+    private Card.Color mapLightToDarkColor(Card.Color lightColor) {
+        switch (lightColor) {
+            case RED: return Card.Color.TEAL;
+            case BLUE: return Card.Color.PURPLE;
+            case GREEN: return Card.Color.PINK;
+            case YELLOW: return Card.Color.ORANGE;
+            default: return lightColor;
+        }
+    }
+
+    /**
+     * Maps a light side number value to its corresponding dark side value
+     */
+    private Card.Value mapLightToDarkValue(Card.Value lightValue) {
+        // Simple mapping: same numbers on both sides
+        return lightValue;
     }
 
     /**
@@ -76,6 +123,12 @@ public class Deck {
         }
         return cards.remove(cards.size() - 1);
     }
+
+    public void flipAllCards() {
+        for (Card card : cards) {
+            card.flip();
+        }
+    }
     
     /**
      * Gets number of cards remaining
@@ -87,6 +140,4 @@ public class Deck {
     public boolean isEmpty() {
         return cards.isEmpty();
     }
-
-
 }
